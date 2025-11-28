791d2758f49cbd213a0bb04d83d3ea615b2d888b
taha <tahamaj4@gmail.com>
2025-11-28 18:08:44 +0330

    Add Jupyter notebook for medical image segmentation using U-Net architecture. Includes dataset preparation, model definition, training loop, evaluation metrics, and visualization of predictions.

--- This_year/CA3/dataset/Q1_dataprep.py
+++ This_year/CA3/dataset/Q1_dataprep.py
@@ -1,31 +1,34 @@
-
 import numpy as np
 import nibabel as nib
 import warnings
 import copy
 import torch
+from torch.utils.data import Dataset
+
+
 def get_slice_data(volume_data, seg_data, axis, slice_idx):
-    
-    if axis == 0:  
+
+    if axis == 0:
         vol_slice = volume_data[slice_idx, :, :]
         seg_slice = seg_data[slice_idx, :, :]
-    elif axis == 1:  
+    elif axis == 1:
         vol_slice = volume_data[:, slice_idx, :]
         seg_slice = seg_data[:, slice_idx, :]
-    else:  
+    else:
         vol_slice = volume_data[:, :, slice_idx]
         seg_slice = seg_data[:, :, slice_idx]
-    
+
     vol_slice = np.squeeze(vol_slice)
     seg_slice = np.squeeze(seg_slice)
-    
+
     vol_slice_rotated = np.rot90(vol_slice, k=1)
     seg_slice_rotated = np.rot90(seg_slice, k=1)
-    
+
     return vol_slice_rotated, seg_slice_rotated
 
+
 def pad_slice(vol_slice, seg_slice, target_dims=(256, 256)):
-    
+
     h, w = vol_slice.shape
     th, tw = target_dims
 
@@ -34,20 +37,22 @@ def pad_slice(vol_slice, seg_slice, target_dims=(256, 256)):
     pad_w_left = (tw - w) // 2
     pad_w_right = tw - w - pad_w_left
 
-    padding = ((max(0, pad_h_top), max(0, pad_h_bottom)), 
-               (max(0, pad_w_left), max(0, pad_w_right)))
+    padding = (
+        (max(0, pad_h_top), max(0, pad_h_bottom)),
+        (max(0, pad_w_left), max(0, pad_w_right)),
+    )
 
-    vol_padded = np.pad(vol_slice, padding, mode='constant', constant_values=0)
-    seg_padded = np.pad(seg_slice, padding, mode='constant', constant_values=0)
-    
+    vol_padded = np.pad(vol_slice, padding, mode="constant", constant_values=0)
+    seg_padded = np.pad(seg_slice, padding, mode="constant", constant_values=0)
+
     vol_padded = vol_padded[:th, :tw]
     seg_padded = seg_padded[:th, :tw]
 
     return vol_padded, seg_padded
 
+
 class IBSRPatchDataset(Dataset):
-    
-    
+
     def __init__(self, volume_files, segmentation_files):
         self.patches = []
         self.masks = []
@@ -68,23 +73,29 @@ class IBSRPatchDataset(Dataset):
 
                 for axis in range(3):
                     num_slices_in_axis = vol_data.shape[axis]
-                    
-                    slice_indices = list(range(
-                        self.slice_start, 
-                        num_slices_in_axis, 
-                        self.slice_stride
-                    ))[:self.num_slices_to_extract]
+
+                    slice_indices = list(
+                        range(self.slice_start, num_slices_in_axis, self.slice_stride)
+                    )[: self.num_slices_to_extract]
 
                     for slice_idx in slice_indices:
-                        vol_slice_rotated, seg_slice_rotated = get_slice_data(vol_data, seg_data, axis, slice_idx)
-                        
-                        vol_padded, seg_padded = pad_slice(vol_slice_rotated, seg_slice_rotated, self.target_dims)
-                        
+                        vol_slice_rotated, seg_slice_rotated = get_slice_data(
+                            vol_data, seg_data, axis, slice_idx
+                        )
+
+                        vol_padded, seg_padded = pad_slice(
+                            vol_slice_rotated, seg_slice_rotated, self.target_dims
+                        )
+
                         for x in [0, self.patch_size]:
                             for y in [0, self.patch_size]:
-                                vol_patch = vol_padded[x : x + self.patch_size, y : y + self.patch_size]
-                                seg_patch = seg_padded[x : x + self.patch_size, y : y + self.patch_size]
-                                
+                                vol_patch = vol_padded[
+                                    x : x + self.patch_size, y : y + self.patch_size
+                                ]
+                                seg_patch = seg_padded[
+                                    x : x + self.patch_size, y : y + self.patch_size
+                                ]
+
                                 self.patches.append(vol_patch)
                                 self.masks.append(seg_patch)
             except Exception as e:
@@ -98,11 +109,12 @@ class IBSRPatchDataset(Dataset):
     def __getitem__(self, idx):
         vol_patch = self.patches[idx].astype(np.float32)
         seg_patch = self.masks[idx].astype(np.int64)
-        
+
         vol_patch_normalized = vol_patch
 
-        
-        vol_tensor = torch.tensor(vol_patch_normalized, dtype=torch.float32).unsqueeze(0)
-        mask_tensor = torch.tensor(seg_patch, dtype=torch.long) 
-        
+        vol_tensor = torch.tensor(vol_patch_normalized, dtype=torch.float32).unsqueeze(
+            0
+        )
+        mask_tensor = torch.tensor(seg_patch, dtype=torch.long)
+
         return vol_tensor, mask_tensor
